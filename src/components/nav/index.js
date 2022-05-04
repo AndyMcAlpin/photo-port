32495c7a81d638aa0ab9c32ca453f3bbec160723
Andy McAlpin <AndrewMcAlpin@gmail.com>
2022-05-04 18:02:09 -0500

    finish contact form

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -34,7 +34,7 @@ function Nav(props) {
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
             >
