c21704937e02bc06cb50d40dcbdd9b7385a162bc
Andy McAlpin <AndrewMcAlpin@gmail.com>
2022-05-04 18:40:46 -0500

    final build

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+afterEach(cleanup)
